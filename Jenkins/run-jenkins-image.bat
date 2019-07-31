7e2e7ac1bb106cdbcad10ece48ba082b51c8489e
gotti2016 <gottfried.luef@at.ibm.com>
2019-07-31 20:12:49 +0200

    things

--- Jenkins/run-jenkins-image.bat
+++ Jenkins/run-jenkins-image.bat
@@ -1,2 +1,10 @@
 docker network create integration_net
-docker run --rm -d -p 8080:8080 -p 50000:50000 -v //var/run/docker.sock:/var/run/docker.sock -v c:\\volumeshare\\jenkins_home:/var/jenkins_home --name jenkins --network="integration_net" jenkins-docker
+docker run^
+ -p 8080:8080 -p 50000:50000^
+ -v //var/run/docker.sock:/var/run/docker.sock^
+ -v c:/volumeshare/jenkins_home:/var/jenkins_home^
+ --name jenkins^
+ --network="integration_net"^
+ --rm^
+ -d^
+ jenkins-docker
