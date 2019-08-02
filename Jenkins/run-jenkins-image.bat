11122c548f3639870148330a4c55b0dc4144c3dd
gotti2016 <gottfried.luef@at.ibm.com>
2019-08-02 14:26:22 +0200

    housekeeping

--- Jenkins/run-jenkins-image.bat
+++ Jenkins/run-jenkins-image.bat
@@ -1,5 +1,6 @@
 docker network create integration_net
 docker run^
+ -u jenkins^
  -p 8080:8080 -p 50000:50000^
  -v //var/run/docker.sock:/var/run/docker.sock^
  -v c:/volumeshare/jenkins_home:/var/jenkins_home^
